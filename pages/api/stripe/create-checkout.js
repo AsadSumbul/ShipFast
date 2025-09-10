a5061a49889eead3fddf96296d29bb8fdfa0b285
AsadSumbul <95354523+AsadSumbul@users.noreply.github.com>
2025-09-10 07:06:48 +0500

    Updated ButtonLead.js

--- pages/api/stripe/create-checkout.js
+++ pages/api/stripe/create-checkout.js
@@ -6,8 +6,14 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // This function is used to create a Stripe Checkout Session
 // It's called by the <ButtonCheckout /> component
-        const { amount, successUrl, cancelUrl, customerEmail, planName } = body;
+export default async function handler(req, res) {
   const { method, body } = req;
+  
+  switch (method) {
+    case "POST": {
+      try {
+        const { amount, successUrl, cancelUrl, customerEmail, planName } = body;
+        
         const stripeSession = await stripe.checkout.sessions.create({
           mode: "payment",
           customer_creation: "always",
@@ -36,31 +42,33 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
             amount: amount.toString(),
           },
         });
-      }
+        
         return res.status(200).json({ url: stripeSession.url });
       } catch (e) {
         console.error(e);
         return res.status(500).json({ error: e?.message });
       }
     }
-    await connectMongo();
+    case "GET": {
+      try {
+        await connectMongo();
 
-          const { coupon, successUrl, cancelUrl } = body;
+        const { coupon, successUrl, cancelUrl } = body;
 
-          const stripeSessionURL = await createCheckout({
-            successUrl,
-            cancelUrl,
-            clientReferenceID: user._id.toString(),
-            priceId: body.priceId,
-            coupon,
-          });
+        const stripeSessionURL = await createCheckout({
+          successUrl,
+          cancelUrl,
+          clientReferenceID: user._id.toString(),
+          priceId: body.priceId,
+          coupon,
+        });
 
-          return res.status(200).json({ url: stripeSessionURL });
-        } catch (e) {
-          console.error(e);
-          return res.status(500).json({ error: e?.message });
-        }
+        return res.status(200).json({ url: stripeSessionURL });
+      } catch (e) {
+        console.error(e);
+        return res.status(500).json({ error: e?.message });
       }
+    }
     default:
       res.status(405).json({ error: "Method not allowed" });
   }
